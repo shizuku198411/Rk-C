2cb51e713e18e5b48e17d9bf55580a2d924d4478
jixeis9541@gmail.com <jixeis9541@gmail.com>
2026-05-05 08:08:49 +0900

    add kernel banner

--- src/kernel.nim
+++ src/kernel.nim
@@ -11,8 +11,23 @@ discard sizeof(trap_types.TrapFrame)
 discard cast[pointer](trap.trapHandler)
 discard cast[pointer](string.strlen)
 
+proc kernelBanner() =
+  putChar('\n')
+  println("╔═══════════════════════════════════╗")
+  println("║  ██████╗  ██╗  ██╗       ██████╗  ║")
+  println("║  ██╔══██╗ ██║ ██╔╝      ██╔════╝  ║")
+  println("║  ██████╔╝ █████╔╝ █████╗██║       ║")
+  println("║  ██╔══██╗ ██╔═██╗ ╚════╝██║       ║")
+  println("║  ██║  ██║ ██║  ██╗      ╚██████╗  ║")
+  println("║  ╚═╝  ╚═╝ ╚═╝  ╚═╝       ╚═════╝  ║")
+  println("╠═══════════════════════════════════╣")
+  println("║  version: 0.1.0                   ║")
+  println("╚═══════════════════════════════════╝")
+
 proc kernel_main*(hartid: U64, dtb: pointer) {.exportc, cdecl.} =
   kernelBootstrap(hartid, dtb)
+
+  kernelBanner()
 
   if createShellUserProcess() < 0:
     panic("failed to create shell")
